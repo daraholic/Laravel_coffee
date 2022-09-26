7289f0f77a9f7ad90cc35343336a4767c287d1eb
daraholic <1112dara@gmail.com>
2022-09-26 23:54:16 +0800

    add vue3 cdn

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -63,5 +63,22 @@
 <h3>Now,if you bought a new bean,add to here</h3>
 <button><a href="/addBean">add</a></button>
 
+<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+<div id="app">@{{ message }}</div>
+
+<script>
+  const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        message: 'Hello Vue!'
+      }
+    }
+  }).mount('#app')
+  
+</script>
+
 </body>
 </html>
